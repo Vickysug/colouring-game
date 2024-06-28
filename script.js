f1c4518ea0c6b3fdd8548efea7acb4b97b221877
unknown <victoriasugden@yahoo.co.uk>
2024-06-28 11:22:07 +0100

    change of code and including css

--- script.js
+++ script.js
@@ -1,38 +1,77 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('coloringCanvas');
-    const ctx = canvas.getContext('2d');
-    const colorPicker = document.getElementById('colorPicker');
+var colour = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
 
-    // Draw a simple image with different regions
-    function drawImage() {
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+// When clicking on colours items
+$(".controls").on("click", "li", function () {
+    //  Deselect aibling elements
+    $(this).siblings().removeClass("selected");
+    //  Select clicked element
+    $(this).addClass("selected");
 
-        // Example regions
-        ctx.fillStyle = 'red';
-        ctx.beginPath();
-        ctx.arc(150, 150, 100, 0, Math.PI * 2);
-        ctx.fill();
+    // Cache current colour
+    colour = $(this).css("background-color");
 
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(300, 300, 100, 100);
-    }
-
-    drawImage();
-
-    canvas.addEventListener('click', (e) => {
-        const rect = canvas.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        const currentColor = colorPicker.value;
-
-        ctx.fillStyle = currentColor;
-        ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI * 2); // Draw a small circle
-        ctx.fill();
-    });
-
-    colorPicker.addEventListener('change', () => {
-        // Additional logic can be added if needed when color is changed
-    });
 });
+
+
+// When New colour is pressed by user
+$("#revealColorSelect").click(function () {
+    // Show colour select or hide the color select
+    changeColor();
+    $("#colorSelect").toggle();
+});
+
+// Update the new colour span
+function changeColor() {
+    var r = $("#red").val();
+    var g = $("#green").val();
+    var b = $("#blue").val();
+    $("#newColor").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
+}
+
+// When new colour sliders change
+$("input[type=range]").change(changeColor);
+
+
+// When add colour is pressed
+$("#addNewColor").click(function () {
+    // Append the colours to the controls
+    var $newColor = $("<li></li>");
+    $newColor.css("background-color", $("#newColor").css("background-color"));
+    $(".controls ul").append($newColor);
+    // Select the new added colour
+    $newColor.click();
+});
+
+// On mouse events on the canvas
+$canvas.mousedown(function (e) {
+    lastEvent = e;
+    mouseDown = true;
+}).mousemove(function (e) {
+    // Draw lines
+    if (mouseDown) {
+        context.beginPath();
+        context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+        context.lineTo(e.offsetX, e.offsetY);
+        context.strokeStyle = colour;
+        context.lineWidth = 5;
+        context.lineCap = 'round';
+        context.stroke();
+        lastEvent = e;
+    }
+}).mouseup(function () {
+    mouseDown = false;
+}).mouseleave(function () {
+    $canvas.mouseup();
+});
+
+// Clear the canvas when button is clicked
+function clear_canvas_width() {
+    var s = document.getElementById("mainCanvas");
+    var w = s.width;
+    s.width = 10;
+    s.width = w;
+}
